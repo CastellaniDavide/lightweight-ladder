49571ecae19f237d84583990f6dc5283e72c9029
DavideC03 <davide.castellani.5.8.03@gmail.com>
2020-09-01 14:39:17 +0200

    v01.01

--- bin/lightweight-ladder.cpp
+++ bin/lightweight-ladder.cpp
@@ -18,12 +18,28 @@
 
 using namespace std;
 
+int N, now, last, h;
+
 int main()
 {
 //  uncomment the following lines if you want to read/write from files
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
+//  freopen("input.txt", "r", stdin);
+//  freopen("output.txt", "w", stdout);
+//  4 4 2 2 5 -> 4
+//  10 2 6 3 7 0 0 7 3 6 4 -> 7
+
   cin >> N;
+  now = 0;
+  h = 0;
+
+  for(int i=0; i < N; i++){
+    last = now;
+    cin >> now;
+    if(now - last > h)
+        h = now - last;
+  }
+
+  cout << h;
 
   return 0;
 }
